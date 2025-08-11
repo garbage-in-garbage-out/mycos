b92a00a5ee5e9d6d912332ab7e5ae92caf122bf1
Jake Hamilton <jake.hamilton@hey.com>
2025-08-10 21:34:05 -0700

    Add global bit offset mapping for links

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -11,5 +11,8 @@ pub use csr::{build_csr, Effect, CSR};
 pub use layout::{
     bit_to_word, clr_bit, connection_table_offset, section_offsets, set_bit, xor_bit, HEADER_BYTES,
 };
-pub use link::{parse_links, validate_links, Link, LinkError};
+pub use link::{
+    build_link_csr, compute_base_offsets, parse_links, validate_links, ChunkOffsets, Link,
+    LinkError,
+};
 pub use scc::{build_internal_graph, scc_ids_and_topo_levels};
