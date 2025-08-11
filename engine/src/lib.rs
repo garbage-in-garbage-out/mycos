b570da966f6b7766daac31f2dbd572cf1d18d163
Jake Hamilton <jake.hamilton@hey.com>
2025-08-10 18:39:46 -0700

    Add section offset calculations

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -4,4 +4,7 @@ pub mod layout;
 pub use chunk::{
     parse_chunk, validate_chunk, Action, Connection, Error, MycosChunk, Section, Trigger,
 };
-pub use layout::{bit_to_word, clr_bit, set_bit, xor_bit};
+pub use layout::{
+    bit_to_word, clr_bit, connection_table_offset, section_offsets, set_bit, xor_bit,
+    HEADER_BYTES,
+};
