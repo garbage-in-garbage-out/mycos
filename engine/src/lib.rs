85b727db3ccfcff97cf60283c4c508532b94fd6f
Jake Hamilton <jake.hamilton@hey.com>
2025-08-12 15:34:58 -0700

    Add placeholder GPU batch evaluation API

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -4,6 +4,7 @@ pub mod crossover;
 pub mod csr;
 pub mod embed;
 pub mod genome;
+pub mod gpu_eval;
 pub mod layout;
 pub mod link;
 pub mod mutations;
@@ -21,6 +22,7 @@ pub use crossover::crossover;
 pub use csr::{build_csr, Effect, CSR};
 pub use embed::{execute_gated_alias, execute_gated_copy, parse_embeds, Embed, EmbedError, IoMode};
 pub use genome::{ChunkGene, ConnGene, Genome, GenomeMeta, LinkGene, ValidationError};
+pub use gpu_eval::{evaluate_batch, Episode, EpisodeMetrics, FitnessResult, Task};
 pub use layout::{
     bit_to_word, clr_bit, connection_table_offset, section_offsets, set_bit, xor_bit, HEADER_BYTES,
 };
