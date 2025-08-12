1310cda1597317c3927e2c712e922ff2dce777af
Jake Hamilton <jake.hamilton@hey.com>
2025-08-12 16:09:22 -0700

    Implement basic evolution loop

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -3,6 +3,7 @@ pub mod cpu_ref;
 pub mod crossover;
 pub mod csr;
 pub mod embed;
+pub mod evolution;
 pub mod genome;
 pub mod gpu_eval;
 pub mod layout;
@@ -23,6 +24,7 @@ pub use chunk::{
 pub use crossover::crossover;
 pub use csr::{build_csr, Effect, CSR};
 pub use embed::{execute_gated_alias, execute_gated_copy, parse_embeds, Embed, EmbedError, IoMode};
+pub use evolution::{run_evolution, Checkpoint, EvoConfig};
 pub use genome::{ChunkGene, ConnGene, Genome, GenomeMeta, LinkGene, ValidationError};
 pub use gpu_eval::{evaluate_batch, Episode, EpisodeMetrics, FitnessResult};
 pub use layout::{
